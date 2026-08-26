4321e2e1745d4f5a8fd6569693ec13cae8df2a0d
24r21a05a0-jpg <24r21a05a0@mlrit.ac.in>
2026-08-26 16:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Puzzle Master Agent")
 # Gemini AI
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=os.getenv("GEMINI_API_KEY"),
     temperature=0.7
 )
 
